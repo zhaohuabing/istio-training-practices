08eb88d671025be7a79e524334692366c23530ef
ubuntu <ubuntu@localhost.localdomain>
2022-08-28 23:24:11 +0800

    update scripts
    Signed-off-by: ubuntu <ubuntu@localhost.localdomain>

--- install-tools.sh
+++ install-tools.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set +e
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo snap install yq
 sudo snap install fx
+
+sudo apt install docker-compose
 alias k=kubectl
+echo  "alias k=kubectl">> ~/.bashrc
+
+bash <(wget -O- get.docker.com)
+sudo gpasswd -a $USER docker
+newgrp docker
