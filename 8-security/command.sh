130e85ee0e8fb56e9d4e2b809db69562d435ab47
zhaohuabing <zhaohuabing@gmail.com>
2022-08-28 21:40:37 +0800

    update scripts
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- 8-security/command.sh
+++ 8-security/command.sh
@@ -6,3 +6,10 @@ curl -v -HHost:foo.bar.com \
     "https://foo.bar.com:8443/productpage"
 
 istioctl proxy-config listener istio-ingressgateway-b7796bc74-gnj74.istio-system --port 8443 -ojson|fx
+cat jason.jwt | cut -d '.' -f2 - | base64 --decode -
+
+python3 ./gen-jwt.py key.pem --expire=3153600000000000000 --sub "jason"  --iss "issuer@zhaohuabing.com" > jason.jwt
+python3 ./gen-jwt.py key.pem --expire=3153600000000000000 --sub "alice"  --iss "issuer@zhaohuabing.com" > alice.jwt
+
+export TOKEN=`cat jason.jwt`
+curl http://150.158.224.216/productpage -H "Authorization: Bearer $TOKEN
