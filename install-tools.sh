8253721ef9b1170c7a62913e9ec5631280adc22f
Huabing Zhao <zhaohuabing@gmail.com>
2022-08-28 21:51:35 +0800

    update scripts
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

---
+++ install-tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo snap install yq
+sudo snap install fx
+alias k=kubectl
