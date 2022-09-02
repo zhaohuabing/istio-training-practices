7b4518b37d674c874e3660d37d9322a99ead7c87
ubuntu <ubuntu@localhost.localdomain>
2022-09-02 10:38:12 +0800

    update scripts

--- install-tools.sh
+++ install-tools.sh
@@ -3,6 +3,9 @@
 set +e
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo apt install -y bash-completion
+source <(kubectl completion bash)
+echo 'source <(kubectl completion bash)' >>~/.bashrc
 sudo apt -y install snapd
 sudo snap install yq
 sudo snap install fx
