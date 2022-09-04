f72624ee16f4efa539a1edcf70f41f8e24f47e06
ubuntu <ubuntu@localhost.localdomain>
2022-09-04 14:54:39 +0800

    update
    Signed-off-by: ubuntu <ubuntu@localhost.localdomain>

--- 10-observability/install-obervability-tools.sh
+++ 10-observability/install-obervability-tools.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/prometheus.yaml -n istio-system
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/grafana.yaml -n istio-system
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/jaeger.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/jaeger.yaml -n istio-system
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/kiali.yaml -n istio-system
 
