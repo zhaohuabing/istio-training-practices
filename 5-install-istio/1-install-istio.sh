f41c7878db68d193e20d2bce67593465ec5c9645
zhaohuabing <zhaohuabing@gmail.com>
2022-11-22 10:51:14 +0800

    update scripts
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- 5-install-istio/1-install-istio.sh
+++ 5-install-istio/1-install-istio.sh
@@ -5,7 +5,8 @@ export ISTIO_VERSION=1.14.4
 
 set -ex
 
-istioctl version || (curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION  sh - && sudo mv $BASE_DIR/istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin/)
+[ -n "$(istioctl version --remote=false |grep $ISTIO_VERSION)" ] || (curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION  sh - && sudo mv $PWD/istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin/)
+
 kubectl create ns istio-system || true
 istioctl install -y -f ${BASE_DIR}/istio-config.yaml
 kubectl label namespace default istio-injection=enabled --overwrite=true
